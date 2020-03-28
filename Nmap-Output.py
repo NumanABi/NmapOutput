0ed12143eeb084ea505fa04e2baf4fb416753f17
M3T1Nnotcode <metingerdan2@gmail.com>
2020-03-28 13:13:09 +0300

    first commit

---
+++ Nmap-Output.py
@@ -0,0 +1,8 @@
+import os
+os.system("nmap metingerdan.com -oX mett1.xml")
+os.system("nmap planetekno.com -oX mett2.xml")
+os.system("nmap instagram.com -oX mett3.xml")
+
+#Nmap output
+#cmd + nmap : Target Website
+#Belirlenen siteler üzerinde nmap ile tarama
